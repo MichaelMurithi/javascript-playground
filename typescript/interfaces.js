078276b9f6b167baa93413c6ce8c15998bdb055d
mykemk <mykemitrichev@gmail.com>
2021-03-27 20:11:20 +0100

    Practiced interfaces with objects

--- typescript/interfaces.js
+++ typescript/interfaces.js
@@ -6,3 +6,19 @@ var automobile = {
     }
 };
 automobile.speedMethod();
+//Implementing interfaces in function
+function car(automobile) {
+    automobile.speed = 500; //Using a value that is not a number raises an error 
+    console.log(automobile.brand + " has " + automobile.speed);
+}
+car(automobile);
+//Implementing interfaces in classes
+var Automobile = /** @class */ (function () {
+    function Automobile() {
+    }
+    Automobile.prototype.speedMethod = function () {
+        console.log("Hi my car is going at " + this.speed);
+    };
+    return Automobile;
+}());
+var car1 = new Automobile();
--- typescript/interfaces.ts
+++ typescript/interfaces.ts
@@ -14,3 +14,28 @@ const automobile: AutomobileInterface ={
 }
 
 automobile.speedMethod()
+
+//Implementing interfaces in function
+function car(automobile:AutomobileInterface){
+    automobile.speed = 500 //Using a value that is not a number raises an error 
+    console.log(`${automobile.brand} has ${automobile.speed}`)
+}
+
+car(automobile)
+
+
+//Implementing interfaces in classes
+class Automobile implements AutomobileInterface{
+    brand:string
+    speed:number
+
+    speedMethod(){
+        console.log(`Hi my car is going at ${this.speed}`)
+    }
+}
+
+const car1 = new Automobile()
+
+interface AutomomobileInterface2 extends AutomobileInterface{
+    
+}
