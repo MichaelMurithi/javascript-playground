abff5395851756b36ddbed09fed90a578a2c1f43
mykemk <mykemitrichev@gmail.com>
2021-03-27 12:03:55 +0100

    Practiced types in function arguments

---
+++ typescript/types2.js
@@ -0,0 +1,11 @@
+function calculate(val1, val2) {
+    /**
+
+ * @param val1 --It is of type number
+ * @param val2  -- of type number
+ * @returns --total which is of type number
+ */
+    var total = val1 + val2; //Makes it more strict by 
+    return total;
+}
+console.log(calculate(12, 14));
---
+++ typescript/types2.ts
@@ -0,0 +1,14 @@
+
+function calculate(val1:number,val2:number) : number{ 
+    /**
+
+ * @param val1 --It is of type number
+ * @param val2  -- of type number
+ * @returns --total which is of type number
+ */
+    let total:number = val1 + val2; //Makes it more strict by 
+    return total
+}
+
+console.log(calculate(12,14))
+
